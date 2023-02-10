667c81d96a460c33ec2395e7c918e954a935e0df
antonbinom <protest_the_hero@mail.ru>
2023-02-10 19:04:30 +0300

    add: ...

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -47,7 +47,7 @@ const authUser = asyncHandler(async (req, res) => {
 
     const user = await User.findOne({ email });
 
-    if (user) {
+    if (user && (await user.matchPassword(password))) {
         res.json({
             _id: user._id,
             name: user.name,
@@ -56,9 +56,9 @@ const authUser = asyncHandler(async (req, res) => {
             token: generateToken(user, _id)
         });
     } else {
-        res.status(400);
-        throw new Error('Failed tot Create the User')
+        res.status(401);
+        throw new Error('Invalid Email or Password')
     }
 });
 
-module.exports = { registerUser }
+module.exports = { registerUser, authUser }
